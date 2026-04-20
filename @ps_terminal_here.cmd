e660c67451bb16dac55a28f4752b9fd43d207919
Richard Carver <121818627+carverrn1@users.noreply.github.com>
2026-04-20 16:00:27 -0500

    Updated @ps_terminal_here to 2 parts so you can double-click the cmd to launch the ps1

---
+++ @ps_terminal_here.cmd
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\PowerShell\7\pwsh.exe" -NoExit -File "%~dp0%~n0.ps1"
--- @ps_terminal_here.ps1
+++ @ps_terminal_here.ps1
@@ -1,7 +1,3 @@
-Start-Process pwsh.exe `
-  -WorkingDirectory $PSScriptRoot `
-  -ArgumentList @(
-    '-NoExit',
-    '-ExecutionPolicy', 'Bypass',
-    '-Command', "& { Set-Location -LiteralPath '$PSScriptRoot'; . '$PSScriptRoot\set-env.ps1'; Write-Host 'Current Branch:' (git branch --show-current) }"
-  )
+Set-Location -LiteralPath $PSScriptRoot
+. "$PSScriptRoot\set-env.ps1"
+Write-Host 'Current Branch:' (git branch --show-current)
